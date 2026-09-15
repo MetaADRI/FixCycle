6a42ebcb8a2711a7717c44ac727065c600d93143
Adrian Mange <adrianmange00@gmail.com>
2026-09-15 12:52:59 +0200

    Register Laravel HandleCors middleware so preflight requests are answered

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
+        \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\Language::class,
         \App\Http\Middleware\ClickJackingPrevention::class,
     ];
